ad92b35615e379c70ab910fcbe814c5b0ce116a7
Vipin Baghel <vipinbaghel1999@gmail.com>
2024-11-16 23:28:23 +0530

    updated UI for transactions view

--- AccountsManagementApp/views/all_transactions.py
+++ AccountsManagementApp/views/all_transactions.py
@@ -7,7 +7,7 @@ from AccountsManagementApp.models.Transaction import Transaction
 class AllTransactionsView(View):
 
     def get(self, request):
-        transactions = Transaction.objects.all()
+        transactions = Transaction.objects.all().order_by("-date")
         return render(
             request,
             "AccountsManagementApp/all_transactions.html",
