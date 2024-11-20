41c56fca42d084987c04ce86d4fd2fe76dfdce4c
Vipin Baghel <vipinbaghel1999@gmail.com>
2024-11-20 23:49:18 +0530

    updated dashboard page with more graphs

--- FinanceManagementApp/views/dashboard.py
+++ FinanceManagementApp/views/dashboard.py
@@ -35,7 +35,8 @@ class DashboardView(View):
                 month=3, day=31
             )
 
-        revenue_this_month = (
+        # Calculate this month's revenue, expense, and profit
+        this_month_revenue = (
             Transaction.objects.filter(
                 date__gte=this_month_start,
                 date__lte=this_month_end,
@@ -44,7 +45,7 @@ class DashboardView(View):
             or 0
         )
 
-        expense_this_month = (
+        this_month_expense = (
             Transaction.objects.filter(
                 date__gte=this_month_start,
                 date__lte=this_month_end,
@@ -53,7 +54,44 @@ class DashboardView(View):
             or 0
         )
 
-        revenue_this_year = (
+        this_month_profit = this_month_revenue - this_month_expense
+
+        # Create a Plotly figure for this month's revenue, expense, and profit
+        fig_this_month = go.Figure(
+            data=[
+                go.Bar(
+                    name="Revenue",
+                    x=["This Month"],
+                    y=[this_month_revenue],
+                    text=[f"{this_month_revenue:,}"],
+                    textposition="auto",
+                ),
+                go.Bar(
+                    name="Expense",
+                    x=["This Month"],
+                    y=[this_month_expense],
+                    text=[f"{this_month_expense:,}"],
+                    textposition="auto",
+                ),
+                go.Bar(
+                    name="Profit",
+                    x=["This Month"],
+                    y=[this_month_profit],
+                    text=[f"{this_month_profit:,}"],
+                    textposition="auto",
+                ),
+            ]
+        )
+        # Update the layout
+        fig_this_month.update_layout(
+            title="This Month's Revenue, Expense, and Profit", barmode="group"
+        )
+
+        # Convert the figure to HTML
+        graph_html_this_month = fig_this_month.to_html(include_plotlyjs="cdn")
+
+        # Calculate this financial year's revenue, expense, and profit
+        this_year_revenue = (
             Transaction.objects.filter(
                 date__gte=this_year_start,
                 date__lte=this_year_end,
@@ -62,7 +100,7 @@ class DashboardView(View):
             or 0
         )
 
-        expense_this_year = (
+        this_year_expense = (
             Transaction.objects.filter(
                 date__gte=this_year_start,
                 date__lte=this_year_end,
@@ -70,6 +108,43 @@ class DashboardView(View):
             ).aggregate(total_amount=Sum("amount"))["total_amount"]
             or 0
         )
+
+        this_year_profit = this_year_revenue - this_year_expense
+
+        # Create a Plotly figure
+        fig_this_year = go.Figure(
+            data=[
+                go.Bar(
+                    name="Revenue",
+                    x=["This Financial Year"],
+                    y=[this_year_revenue],
+                    text=[f"{this_year_revenue:,}"],
+                    textposition="auto",
+                ),
+                go.Bar(
+                    name="Expense",
+                    x=["This Financial Year"],
+                    y=[this_year_expense],
+                    text=[f"{this_year_expense:,}"],
+                    textposition="auto",
+                ),
+                go.Bar(
+                    name="Profit",
+                    x=["This Financial Year"],
+                    y=[this_year_profit],
+                    text=[f"{this_year_profit:,}"],
+                    textposition="auto",
+                ),
+            ]
+        )
+
+        # Update the layout
+        fig_this_year.update_layout(
+            title="This Financial Year's Revenue, Expense, and Profit", barmode="group"
+        )
+
+        # Convert the figure to HTML
+        graph_html_this_year = fig_this_year.to_html(include_plotlyjs="cdn")
 
         # Get project-wise income and expense data, 5 most recent by start date
         project_data = (
@@ -82,7 +157,10 @@ class DashboardView(View):
             .reverse()[:5]
         )
 
-        # Create a Plotly figure
+        #  list to store profit values
+        profit = [data["income"] - data["expense"] for data in project_data]
+
+        # Create a Plotly figure for project-wise income, expense, and profit
         fig = go.Figure(
             data=[
                 go.Bar(
@@ -99,25 +177,32 @@ class DashboardView(View):
                     text=[f"{data['expense']:,}" for data in project_data],
                     textposition="auto",
                 ),
+                go.Bar(
+                    name="Profit",
+                    x=[data["project__name"] for data in project_data],
+                    y=profit,
+                    text=[f"{value:,}" for value in profit],
+                    textposition="auto",
+                ),
             ]
         )
 
         # Update the layout
-        fig.update_layout(barmode="group")
+        fig.update_layout(
+            title="Project-wise Income, Expense, and Profit", barmode="group"
+        )
 
         # Convert the figure to HTML
-        graph_html = fig.to_html(include_plotlyjs="cdn")
+        graph_html_project_wise = fig.to_html(include_plotlyjs="cdn")
 
         # Render the dashboard template
         return render(
             request,
             "FinanceManagementApp/dashboard.html",
             {
-                "revenue_this_month": revenue_this_month,
-                "expense_this_month": expense_this_month,
-                "revenue_this_year": revenue_this_year,
-                "expense_this_year": expense_this_year,
                 "recent_transactions": recent_transactions,
-                "graph_html": graph_html,
+                "graph_html_project_wise": graph_html_project_wise,
+                "graph_html_this_month": graph_html_this_month,
+                "graph_html_this_year": graph_html_this_year,
             },
         )
